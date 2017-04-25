8711f422fac3a38062e918cdb78dfea3ba932be3
Peter Barcsak <peter.barcsak@gmail.com>
2017-04-25 16:44:33 +0100

    Test environment up and running

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-    get '/' do
-    "Hello Battle!"
-  end
+
+  get '/' do
+      erb :index
+    end
+
+    post '/names' do
+      @name1 = params[:name1]
+      @name2 = params[:name2]
+      erb :play
+    end
   run! if app_file == $0
 end
